bc4867b65ef4404af270d9b4d7935231f9d21f7b
JakobHuemer <114645930+JakobHuemer@users.noreply.github.com>
2023-04-08 21:20:31 +0200

    Delete certificate-options.js

--- backend/certificate-options.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export let options = {
-    key: fs.readFileSync('./backend/ssl/localhost.key'),
-    cert: fs.readFileSync('./backend/ssl/localhost.crt'),
-    passphrase: "einsbaum"
-}
-
