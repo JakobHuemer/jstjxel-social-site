1ba03fc51eeef4ab0aa35ff1f21ecf74e6d36ff4
Jakob Huemer-Fistelberger <j.huemer-fistelberger@students.htl-leonding.ac.at>
2023-04-09 22:10:36 +0200

    changed twitch channel to jstjxel

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,8 @@ const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 const oauthToken = process.env.TWITCH_CLIENT_OAUTH_TOKEN;
 const clientUserName = process.env.TWITCH_CLIENT_USERNAME;
-const channel = 'jakkibot';
-// const channel = 'jixel';
+// const channel = 'jakkibot';
+const channel = 'jixel';
 
 const twitchChatBot = new TwitchChatBot(
                 clientId,
